f518b6efabe6792f509b31a810afda4f91ede350
josephm <josephm@strangeanimals.com.au>
2019-07-22 19:06:48 +0800

    basic coming soon page

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img class="beer" alt="Beer" src="./assets/beer.png">
+    <img class="footy" alt="Footy" src="./assets/footy.jpg">
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+
+    <ComingSoon msg="Welcome to BeersAndFooty.com"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ComingSoon from './components/ComingSoon.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    ComingSoon
   }
 }
 </script>
@@ -24,5 +31,17 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  .beer, .footy{
+    transition: transform 0.3s ease-out;
+    width: 20%;
+    height: auto;
+    z-index: 0;
+  }
+  .beer:hover{
+    transform: rotate(-180deg);
+  }
+  .footy:hover{
+    transform: rotate(180deg);
+  }
 }
 </style>
---
+++ src/components/ComingSoon.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="coming-soon uk-container">
+    <h1>{{ msg }}</h1>
+    <p>
+      The website is still in development, please come back soon!
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
