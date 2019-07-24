9b4ff846fe7a3be63fb81ed55b60a00cea55e00f
josephm <josephm@strangeanimals.com.au>
2019-07-24 19:37:41 +0800

    deploy.sh changes

--- deploy.sh
+++ deploy.sh
@@ -4,21 +4,12 @@ set -e
 # build
 npm run build
 
-# commit changes
-git commit -m 'deploy'
-
 # navigate into the build output directory
 cd dist
 
 # if you are deploying to a custom domain
 echo 'beersandfooty.com' > CNAME
 
-git init
-git add -A
-git commit -m 'deploy'
-
 cd -
-
-git push
 
 npm run deploy
