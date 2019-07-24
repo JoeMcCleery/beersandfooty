b33a31291dd3530d746d9ce0fee711407739a5f1
josephm <josephm@strangeanimals.com.au>
2019-07-24 21:51:41 +0800

    set fixed view height to window height

--- src/App.vue
+++ src/App.vue
@@ -29,7 +29,9 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  height: 100%;
+  height: 100vh;
   width: 100%;
+  margin: 0;
+  padding: 0;
 }
 </style>
