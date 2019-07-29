052498bebf9f12585b3045bef3dd6212faf711a4
josephm <josephm@strangeanimals.com.au>
2019-07-29 22:48:51 +0800

    deploy update

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,12 @@
 # abort on errors
 set -e
 
-# build
-npm run build
-
-# get latest version and push commits
+# get latest version
 git fetch
 git pull
-git push
+
+# build
+npm run build
 
 # navigate into the build output directory
 cd dist
