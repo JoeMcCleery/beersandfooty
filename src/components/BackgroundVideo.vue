66a3cae064ee9a8ffde9186cbe32ef9aee06f33a
josephm <josephm@strangeanimals.com.au>
2019-07-29 22:21:29 +0800

    bg video dev

---
+++ src/components/BackgroundVideo.vue
@@ -0,0 +1,30 @@
+<template>
+<div class="background">
+    <div class="backgroundVideo uk-position-center">
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'BackgroundVideo',
+  props: {
+    vimeoID: String
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.background {
+    height: 100vh;
+    width: 100vw;
+    z-index: -1;
+    position: absolute;
+    top: 0;
+    left: 0;
+    overflow: hidden;
+    .backgroundVideo {
+        background-color: rgba(0,0,0,0.01);
+    }
+}
+</style>
