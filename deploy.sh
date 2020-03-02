58fc5af83fa4c9b5ae44d9085523f168dbd97fdf
JoeMcCleery <mccleery.joseph@gmail.com>
2020-03-02 16:58:59 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 # abort on errors
 set -e
 
-git remote add origin 'git+https://'$GH_TOKEN':x-oauth-basic@github.com/joemccleery/beersandfooty.git'
-git config --global git.protocol git+https
+git remote add origin 'https://$GH_TOKEN:x-oauth-basic@github.com/joemccleery/beersandfooty.git'
+git config --global git.protocol https
 git config --global user.email "mccleery.joseph@gmail.com"
 git config --global user.name "joemccleery"
 
