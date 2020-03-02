3f1adbef8d8fe0135e056db22454d9add914a868
JoeMcCleery <mccleery.joseph@gmail.com>
2020-03-02 15:41:16 +0800

    added repo to gh-pages deploy for cli support

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var ghpages = require('gh-pages')
 
-ghpages.publish('dist', deployed())
+ghpages.publish('dist', {
+    branch: 'master',
+    repo: 'https://github.com/JoeMcCleery/beersandfooty.git'
+  }, deployed());
 
 function deployed () {
-    
     return;
 }
