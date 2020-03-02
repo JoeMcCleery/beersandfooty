1518d27a8a70f9dd8137a782252e4cbbf1d3fc75
JoeMcCleery <mccleery.joseph@gmail.com>
2020-03-02 16:02:56 +0800

    added git user details to gh-pages js

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ var ghpages = require('gh-pages')
 
 ghpages.publish('dist', {
     branch: 'master',
-    repo: 'https://github.com/JoeMcCleery/beersandfooty.git'
+    repo: 'https://github.com/JoeMcCleery/beersandfooty.git',
+    user: {
+      name: 'joemccleery',
+      email: 'mccleery.joseph@gmail.com'
+    }
   }, deployed());
 
 function deployed () {
