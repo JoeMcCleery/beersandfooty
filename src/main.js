fda9a33a822a74b5fac0c5d1d28ae7a3cb6d9da7
JoeMcCleery <mccleery.joseph@gmail.com>
2020-04-14 19:17:20 +0800

    master: -add scroll behaviour for vue router (for when hash links are eventually used)

--- src/main.js
+++ src/main.js
@@ -16,10 +16,44 @@ const routes = [
   { path: '/footy', component: Website }
 ]
 
+// scrollBehavior:
+// - only available in html5 history mode
+// - defaults to no scroll behavior
+// - return false to prevent scroll
+const scrollBehavior = (to, from, savedPosition) => {
+  if (savedPosition) {
+    // savedPosition is only available for popstate navigations.
+    return savedPosition
+  } else {
+    const position = {}
+    // new navigation.
+    // scroll to anchor by returning the selector
+    if (to.hash) {
+      position.selector = to.hash
+
+      // specify offset of the element
+      if (to.hash === '#anchor2') {
+        position.offset = { y: 100 }
+      }
+    }
+    // check if any matched route config has meta that requires scrolling to top
+    if (to.matched.some(m => m.meta.scrollToTop)) {
+      // cords will be used if no selector is provided,
+      // or if the selector didn't match any element.
+      position.x = 0
+      position.y = 0
+    }
+    // if the returned position is falsy or an empty object,
+    // will retain current scroll position.
+    return position
+  }
+}
+
 // Create Router Instance
 const router = new VueRouter({
   mode: 'history',
-  routes
+  routes,
+  scrollBehavior
 })
 
 // Render App
