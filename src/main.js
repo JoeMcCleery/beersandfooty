d9583213fae47f9d12ce85aebfa31c648c7687fd
JoeMcCleery <mccleery.joseph@gmail.com>
2020-04-14 14:17:13 +0800

    master: -add sitemap generator

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import VueRouterSitemap from 'vue-router-sitemap'
+import path from 'path'
 import App from './App'
 
 // Import Components for routing
@@ -21,6 +23,27 @@ const router = new VueRouter({
   mode: 'history',
   routes
 })
+
+const sitemapMiddleware = () => {
+  return (req, res) => {
+    res.set('Content-Type', 'application/xml')
+
+    const staticSitemap = path.resolve('dist/static', 'sitemap.xml')
+    const filterConfig = {
+      isValid: false,
+      rules: [
+        /\/example-page/,
+        /\*/
+      ]
+    }
+
+    new VueRouterSitemap(router).filterPaths(filterConfig).build('http://example.com').save(staticSitemap)
+
+    return res.sendFile(staticSitemap)
+  }
+}
+
+App.get('/sitemap.xml', sitemapMiddleware())
 
 // Render App
 /* eslint-disable no-new */
