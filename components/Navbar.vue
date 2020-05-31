78a0079cca44cd021c777a2261df7ca2d8db5251
JoeMcCleery <mccleery.joseph@gmail.com>
2020-05-31 15:44:03 +0800

    convert-website-to-nuxt: -fix weird nav issue

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,91 +1,89 @@
 <template>
-  <div class="uk-position-top">
-    <nav
-      id="navbar"
-      class="uk-navbar-container uk-dark uk-navbar-transparent"
-      uk-navbar
-    >
-      <div class="uk-navbar-right">
-        <a
-          href="#offcanvas-slide"
-          class="uk-button uk-button-default uk-hidden@s"
-          uk-toggle
-        >
-          <span>&nbsp;{{ title }}</span>
-          <span uk-icon="icon: menu; ratio: 1;" />
-        </a>
-        <ul class="uk-navbar-nav uk-visible@s">
-          <li :class="{ 'uk-active': title === 'Home' }">
-            <nuxt-link to="/" class="uk-navbar-item">Home</nuxt-link>
-          </li>
-          <li class="uk-nav-divider"></li>
-          <li :class="{ 'uk-active': title === 'Beer' }">
-            <nuxt-link to="/beer" class="uk-navbar-item">Beer</nuxt-link>
-          </li>
-          <li :class="{ 'uk-active': title === 'Footy' }">
-            <nuxt-link to="/footy" class="uk-navbar-item">Footy</nuxt-link>
-          </li>
-          <li class="uk-nav-divider"></li>
-          <li :class="{ 'uk-active': title === 'About' }">
-            <nuxt-link to="/about" class="uk-navbar-item">About</nuxt-link>
-          </li>
-          <!-- Auth Routes -->
-          <li v-if="user" :class="{ 'uk-active': title === 'Account' }">
-            <nuxt-link to="/account" class="uk-navbar-item">
-              Account
-            </nuxt-link>
-          </li>
-          <li v-else>
-            <a href="#login-modal" class="uk-navbar-item" uk-toggle>
-              Login / Register
-            </a>
-          </li>
-        </ul>
-        <div
-          id="offcanvas-slide"
-          uk-offcanvas="overlay: true; mode: slide; esc-close: true; flip: true;"
-        >
-          <div class="uk-offcanvas-bar">
-            <button class="uk-offcanvas-close" type="button" uk-close></button>
-            <ul class="uk-nav uk-nav-primary uk-nav-center">
-              <li :class="{ 'uk-active': title === 'Home' }">
-                <nuxt-link to="/" uk-toggle="target: #offcanvas-slide;"
-                  >Home</nuxt-link
-                >
-              </li>
-              <li class="uk-nav-divider"></li>
-              <li :class="{ 'uk-active': title === 'Beer' }">
-                <nuxt-link to="/beer" uk-toggle="target: #offcanvas-slide;"
-                  >Beer</nuxt-link
-                >
-              </li>
-              <li :class="{ 'uk-active': title === 'Footy' }">
-                <nuxt-link to="/footy" uk-toggle="target: #offcanvas-slide;"
-                  >Footy</nuxt-link
-                >
-              </li>
-              <li class="uk-nav-divider"></li>
-              <li :class="{ 'uk-active': title === 'About' }">
-                <nuxt-link to="/about" uk-toggle="target: #offcanvas-slide;"
-                  >About</nuxt-link
-                >
-              </li>
-              <!-- Auth Routes -->
-              <li v-if="user" :class="{ 'uk-active': title === 'Account' }">
-                <nuxt-link to="/account" uk-toggle="target: #offcanvas-slide;"
-                  >Account</nuxt-link
-                >
-              </li>
-              <li v-else>
-                <a href="#login-modal" uk-toggle>
-                  Login / Register
-                </a>
-              </li>
-            </ul>
+  <div
+    uk-sticky="animation: uk-animation-fade; sel-target: .uk-navbar-container; cls-active: uk-navbar-sticky uk-box-shadow-medium; cls-inactive: uk-navbar-transparent; show-on-up: true;"
+  >
+    <div class="uk-position-top">
+      <nav
+        id="navbar"
+        class="uk-navbar-container uk-dark uk-navbar-transparent"
+        uk-navbar
+      >
+        <div class="uk-navbar-right">
+          <a
+            href="#offcanvas-slide"
+            class="uk-button uk-button-default uk-hidden@s"
+            uk-toggle
+          >
+            <span>&nbsp;{{ title }}</span>
+            <span uk-icon="icon: menu; ratio: 1;" />
+          </a>
+          <ul class="uk-navbar-nav uk-visible@s">
+            <li :class="{ 'uk-active': title === 'Home' }">
+              <nuxt-link to="/" class="uk-navbar-item">Home</nuxt-link>
+            </li>
+            <li class="uk-nav-divider"></li>
+            <li :class="{ 'uk-active': title === 'Beer' }">
+              <nuxt-link to="/beer" class="uk-navbar-item">Beer</nuxt-link>
+            </li>
+            <li :class="{ 'uk-active': title === 'Footy' }">
+              <nuxt-link to="/footy" class="uk-navbar-item">Footy</nuxt-link>
+            </li>
+            <li class="uk-nav-divider"></li>
+            <li :class="{ 'uk-active': title === 'About' }">
+              <nuxt-link to="/about" class="uk-navbar-item">About</nuxt-link>
+            </li>
+            <!-- Auth Routes -->
+            <li v-if="user" :class="{ 'uk-active': title === 'Account' }">
+              <nuxt-link to="/account" class="uk-navbar-item">
+                Account
+              </nuxt-link>
+            </li>
+            <li v-else>
+              <a href="#login-modal" class="uk-navbar-item" uk-toggle>
+                Login / Register
+              </a>
+            </li>
+          </ul>
+          <div
+            id="offcanvas-slide"
+            uk-offcanvas="overlay: true; mode: slide; esc-close: true; flip: true;"
+          >
+            <div class="uk-offcanvas-bar">
+              <button
+                class="uk-offcanvas-close"
+                type="button"
+                uk-close
+              ></button>
+              <ul class="uk-nav uk-nav-primary uk-nav-center">
+                <li :class="{ 'uk-active': title === 'Home' }">
+                  <nuxt-link to="/">Home</nuxt-link>
+                </li>
+                <li class="uk-nav-divider"></li>
+                <li :class="{ 'uk-active': title === 'Beer' }">
+                  <nuxt-link to="/beer">Beer</nuxt-link>
+                </li>
+                <li :class="{ 'uk-active': title === 'Footy' }">
+                  <nuxt-link to="/footy">Footy</nuxt-link>
+                </li>
+                <li class="uk-nav-divider"></li>
+                <li :class="{ 'uk-active': title === 'About' }">
+                  <nuxt-link to="/about">About</nuxt-link>
+                </li>
+                <!-- Auth Routes -->
+                <li v-if="user" :class="{ 'uk-active': title === 'Account' }">
+                  <nuxt-link to="/account">Account</nuxt-link>
+                </li>
+                <li v-else>
+                  <a href="#login-modal" uk-toggle>
+                    Login / Register
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </div>
   </div>
 </template>
 
--- nuxt.config.js
+++ nuxt.config.js
@@ -64,39 +64,7 @@ export default {
   /*
    ** Router configuration
    */
-  router: {
-    scrollBehavior: async (to, from, savedPosition) => {
-      if (savedPosition) {
-        return savedPosition
-      }
-
-      // eslint-disable-next-line require-await
-      const findEl = async (hash, x) => {
-        return (
-          document.querySelector(hash) ||
-          new Promise((resolve, reject) => {
-            if (x > 50) {
-              return resolve()
-            }
-            setTimeout(() => {
-              resolve(findEl(hash, ++x || 1))
-            }, 100)
-          })
-        )
-      }
-
-      if (to.hash) {
-        const el = await findEl(to.hash)
-        if ('scrollBehavior' in document.documentElement.style) {
-          return window.scrollTo({ top: el.offsetTop, behavior: 'smooth' })
-        } else {
-          return window.scrollTo(0, el.offsetTop)
-        }
-      }
-
-      return { x: 0, y: 0 }
-    }
-  },
+  router: {},
   /*
    ** Build configuration
    */
