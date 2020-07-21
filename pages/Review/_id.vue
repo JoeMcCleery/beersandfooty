73317f301a8ae2258070e9b51101dccb4da5f640
JoeMcCleery <mccleery.joseph@gmail.com>
2020-07-21 11:40:19 +0800

    convert-website-to-nuxt: -add more review icons on own page

--- pages/Review/_id.vue
+++ pages/Review/_id.vue
@@ -29,6 +29,69 @@
               <span uk-icon="user" />
             </nuxt-link>
           </div>
+          <div v-if="reviewData.status === 'hidden'">
+            <a title="review hidden">
+              <img data-src="@/assets/images/hide.svg" uk-svg />
+            </a>
+          </div>
+          <div v-if="userCanEditReview">
+            <button class="no-button" title="review options" type="button">
+              <img uk-icon="icon: more-vertical;" />
+            </button>
+            <div
+              uk-dropdown="animation: uk-animation-slide-top-small; duration: 200; mode: click; pos: bottom-justify;"
+            >
+              <ul class="uk-nav uk-dropdown-nav">
+                <li>
+                  <a
+                    class="uk-text-small"
+                    title="Edit Review"
+                    @click="editReviewModal"
+                  >
+                    <span uk-icon="icon: pencil;" />
+                    Edit
+                  </a>
+                </li>
+                <li>
+                  <a
+                    class="uk-text-small"
+                    title="Delete Review"
+                    @click="deleteReview()"
+                  >
+                    <span uk-icon="icon: trash;" />
+                    Delete
+                  </a>
+                </li>
+                <li v-if="isAdmin">
+                  <a
+                    class="uk-text-small"
+                    :title="
+                      reviewData.status === 'published'
+                        ? 'Hide Review'
+                        : 'Show Review'
+                    "
+                    @click="toggleReviewStatus"
+                  >
+                    <img
+                      v-if="reviewData.status === 'published'"
+                      data-src="@/assets/images/hide.svg"
+                      uk-svg
+                    />
+                    <img
+                      v-else-if="reviewData.status === 'hidden'"
+                      data-src="@/assets/images/eye.svg"
+                      uk-svg
+                    />
+                    {{
+                      reviewData.status === 'published'
+                        ? 'Hide Review'
+                        : 'Show Review'
+                    }}
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
       <!--  Page Content Container  -->
@@ -76,6 +139,15 @@ export default {
     },
     userReview() {
       return this.user && this.review.user_id === this.user.id
+    },
+    isAdmin() {
+      return this.user && this.user.isAdmin
+    },
+    userCanEditReview() {
+      return (this.userReview || this.isAdmin) && this.loggedIn
+    },
+    loggedIn() {
+      return this.$store.state.userAccessToken && this.$store.state.user
     }
   },
   async mounted() {
@@ -85,6 +157,52 @@ export default {
       })
       if (result) {
         this.reviewData = result
+      }
+    }
+  },
+  methods: {
+    async toggleReviewStatus() {
+      if (!this.submitting) {
+        this.submitting = true
+        let newStatus = ''
+        if (this.reviewData.status === 'hidden') {
+          newStatus = 'published'
+        } else {
+          newStatus = 'hidden'
+        }
+        try {
+          await this.$store.dispatch('updateReview', {
+            review_id: this.review.id,
+            title: this.review.title,
+            type: this.review.type,
+            publish_date: this.review.publish_date,
+            content_blocks: this.review.content_blocks,
+            status: newStatus
+          })
+          this.submitting = false
+          window.location.reload(true)
+        } catch (e) {
+          this.submitting = false
+          return e
+        }
+      }
+    },
+    editReviewModal() {
+      const modal = document.querySelector('#review-form-modal')
+      if (this.reviewData && modal) {
+        this.$store.dispatch('setEditReview', {
+          review: this.reviewData
+        })
+        this.$uikit.modal(modal).show()
+      }
+    },
+    deleteReview() {
+      const reviewID = this.reviewData.id
+      if (reviewID) {
+        this.$store.dispatch('deleteReview', {
+          id: reviewID
+        })
+        window.location.reload(true)
       }
     }
   },
